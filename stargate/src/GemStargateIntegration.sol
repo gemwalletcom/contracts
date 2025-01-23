3ca64673afeb2c91c17c84da5302b37eff91caa1
kuchmenko <ivan@kuchmenko.com>
2025-01-23 19:26:47 +0200

    feat: added stargate integration

--- stargate/src/GemStargateIntegration.sol
+++ stargate/src/GemStargateIntegration.sol
@@ -2,13 +2,15 @@ pragma solidity ^0.8.19;
 
 import {IStargate} from "@stargatefinance/stg-evm-v2/src/interfaces/IStargate.sol";
 import {MessagingFee, OFTReceipt, SendParam} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
+import {OptionsBuilder} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/libs/OptionsBuilder.sol";
 
 contract StargateIntegration {
     function prepareTakeTaxi(
         address _stargate,
         uint32 _dstEid,
         uint256 _amount,
-        address _receiver
+        address _composer,
+        bytes memory _composeMsg
     )
         external
         view
@@ -18,13 +20,21 @@ contract StargateIntegration {
             MessagingFee memory messagingFee
         )
     {
+        bytes memory extraOptions = _composeMsg.length > 0
+            ? OptionsBuilder.newOptions().addExecutorLzComposeOption(
+                0,
+                200_000,
+                0
+            ) // compose gas limit
+            : bytes("");
+
         sendParam = SendParam({
             dstEid: _dstEid,
-            to: addressToBytes32(_receiver),
+            to: addressToBytes32(_composer),
             amountLD: _amount,
             minAmountLD: _amount,
-            extraOptions: new bytes(0),
-            composeMsg: new bytes(0),
+            extraOptions: extraOptions,
+            composeMsg: _composeMsg,
             oftCmd: ""
         });
 
