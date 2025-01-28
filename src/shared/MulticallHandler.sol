c3458dafe6971791b2eab00e7ea969fbfbbae3f9
Ivan Kuchmenko <44723550+kuchmenko@users.noreply.github.com>
2025-01-28 12:21:25 +0200

    Update src/shared/MulticallHandler.sol
    Co-authored-by: Ferman <119309671+0xh3rman@users.noreply.github.com>

--- src/shared/MulticallHandler.sol
+++ src/shared/MulticallHandler.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.19;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import {ILayerZeroComposer} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroComposer.sol";
-import {OFTComposeMsgCodec} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/libs/OFTComposeMsgCodec.sol";
 
 /**
  * @title MulticallsHandler
